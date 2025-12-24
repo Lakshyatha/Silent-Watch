ca266190d47e034eeaa983a4e2d87bca6fb90c74
Lakshyatha <clakshyatha@gmail.com>
2025-12-24 15:16:06 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { AuthProvider } from './hooks/useAuth';
+import './index.css';
 
-const rootElement = document.getElementById("root");
-
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <h1 style={{ textAlign: "center", marginTop: "40vh" }}>
-        Silent Watch is Live 🚀
-      </h1>
-    </StrictMode>
-  );
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </StrictMode>
+);
